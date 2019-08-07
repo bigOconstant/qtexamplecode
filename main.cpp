9b1587dd09a07f15c53e24d7c6be9323a3c2142b
Caleb McCarthy <caleb.andrew.mccarthy@ibm.com>
2019-08-07 08:45:25 -0500

    Simple QT example

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <iostream>
+#include "plusminus.h"
+using namespace std;
+class MyButton : public QWidget {
+    
+ public:
+   MyButton(QWidget *parent = 0)
+    : QWidget(parent) {
+           
+  QPushButton *quitBtn = new QPushButton("Quit", this);
+  quitBtn->setGeometry(50, 40, 75, 30);
+  std::cout <<" callin this"<<std::endl;
+
+  connect(quitBtn, &QPushButton::clicked, qApp, &QApplication::quit);
+   };
+   
+};
+
+
+int main(int argc, char *argv[]) {
+    
+  QApplication app(argc, argv);  
+    
+  PlusMinus window;
+
+  window.resize(300, 190);
+  window.setWindowTitle("Plus minus");
+  window.show();
+
+  return app.exec();
+}
